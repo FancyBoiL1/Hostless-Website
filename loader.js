3775304b35e626318a11524ca1b1a87d3bb78dde
FancyBoiL1 <117115118+FancyBoiL1@users.noreply.github.com>
2024-03-06 10:54:30 -0800

    Update loader.js
    Moved CSS to be added in the loader

--- loader.js
+++ loader.js
@@ -1,4 +1,8 @@
 function createIframe(dataUrl){
+  const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = 'https://cdn.jsdelivr.net/gh/FancyBoiL1/Completely-Dynamic-Website@master/loader.css';
+  /* CSS to remove annoying iframe border.*/
   var iframe = document.createElement('iframe');   
   iframe.sandbox.add('allow-top-navigation');
   iframe.sandbox.add('allow-same-origin');
@@ -14,7 +18,8 @@ function createIframe(dataUrl){
   iframe.src = dataUrl;
   iframe.allowfullscreen = 'true';
   iframe.allowtransparency='true';
-  document.body.appendChild(iframe);
+  document.getElementsByTagName("head")[0].appendChild(css);
+  document.getElementsByTagName("body")[0].appendChild(iframe);
   window.setInterval(() =>{
     if(iframe.style.width !== window.innerWidth){iframe.style.width = window.innerWidth;}
     if(iframe.style.height !== window.innerHeight){iframe.style.height = window.innerHeight;}
